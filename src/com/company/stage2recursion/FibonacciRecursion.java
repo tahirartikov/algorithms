f848056e50b87fc82a03e1b49a8ad5d0c08d8862
tohir ortiqov <tohirortiqov@gmail.com>
2022-05-11 19:44:46 +0500

    Solution to merge sort

--- src/com/company/stage2recursion/FibonacciRecursion.java
+++ src/com/company/stage2recursion/FibonacciRecursion.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class FibonacciRecursion {
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
 
