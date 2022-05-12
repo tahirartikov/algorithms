c47aae6158586ed4632cb1e6fe0c4454c1a9f832
tohir ortiqov <tohirortiqov@gmail.com>
2022-05-12 17:10:02 +0500

    solution to deep Count task

---
+++ src/com/company/stage2recursion/deepCount.java
@@ -0,0 +1,31 @@
+package com.company.stage2recursion;
+
+import java.lang.reflect.Array;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class deepCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Object[] objects = new Object[]{"x", "y",new Object[] {"z"}};
+        for (Object o : objects) {
+            count(o);
+        }
+        System.out.println(cnt);
+    }
+
+
+    static int cnt = 0;
+
+    static <T> void count(T obs){
+        if(Objects.isNull(obs))
+            return;
+        cnt++;
+        if(obs.getClass().isArray()){
+            for(int i=0; i< Array.getLength(obs); i++) {
+                count(Array.get(obs, i));
+            }
+        }
+    }
+}
