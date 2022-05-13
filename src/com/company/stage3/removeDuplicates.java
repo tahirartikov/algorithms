f4199e6e198b8031eb3c4381c881ea7c76b09c9f
tohir ortiqov <tohirortiqov@gmail.com>
2022-05-13 11:32:57 +0500

    solution to remove duplicates

---
+++ src/com/company/stage3/removeDuplicates.java
@@ -0,0 +1,20 @@
+package com.company.stage3;
+
+public class removeDuplicates {
+
+    public static int removeDuplicateElements( int[] nums){
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                continue;
+            }
+
+            nums[count] = nums[i];
+            count++;
+        }
+        return count;
+    }
+
+}
