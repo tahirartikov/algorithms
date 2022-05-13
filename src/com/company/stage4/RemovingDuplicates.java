29d62dc153b3da7cf865739b4149fb5704c6818d
tohir ortiqov <tohirortiqov@gmail.com>
2022-05-13 18:00:09 +0500

    solution to unique elements in array

--- src/com/company/stage4/RemovingDuplicates.java
+++ src/com/company/stage4/RemovingDuplicates.java
@@ -1,25 +1,20 @@
 package com.company.stage4;
 
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class RemovingDuplicates {
     public static void main(String[] args) {
 
-        List<Integer> list=new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         list.addAll(Arrays.asList(1, 5, 2, 0, 2, -3, 1, 10));
         System.out.println(uniqueElements(list));
     }
 
-    public static List<Integer> uniqueElements(List<Integer> list){
+    public static List<Integer> uniqueElements(List<Integer> list) {
 
-        Set<Integer> set=new LinkedHashSet<>();
+        Set<Integer> set = new LinkedHashSet<>(list);
+        return set.stream().collect(Collectors.toList());
 
-        set.addAll(list);
-        List<Integer> removedList= new ArrayList<>();
-
-        removedList.addAll(set);
-
-        return removedList;
     }
 }
