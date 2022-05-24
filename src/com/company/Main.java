24f8ab380eaf4776bdd05335f13d9f34499b558b
tohir ortiqov <tohirortiqov@gmail.com>
2022-05-24 14:18:32 +0500

    solution to subArraySum and hour-minute angle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        boolean b=isPrime(a);
+        System.out.println(b);
+
     }
+
+    public static boolean isPrime(int n){
+//
+//        if (n == 0 || n==1) {
+//            return false;
+//        }
+//        else if (n==2 || n==3){
+//            return true;
+//        }
+//        else if ((n*n-1)%24==0){
+//            return true;
+//        }
+//        return false;
+
+        int sqrt=(int) Math.sqrt(n) +1;
+        for (int i = 2; i < sqrt; i++) {
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
 
